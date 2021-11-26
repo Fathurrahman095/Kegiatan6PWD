bddca5c79702fb57f270648832f8c2e3ba5ec453
1900018091 <63111228+Fathurrahman095@users.noreply.github.com>
2021-11-26 23:44:41 +0700

    Add files via upload

---
+++ form_user.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2>pendaftaran test covid pada RS. UAD</h2>
+<form method=post action=input_user.php>
+<table>
+<tr><td>No KTP</td><td> : <input name='id_user' type='text'></td></tr>
+<tr><td>Nama Lengkap</td><td> : <input name='nama' type='text'></td></tr>
+<tr><td>Gender</td><td> : <input name='password' type='text'></td></tr>
+<tr><td>Email </td><td> : <input name='email' type='text'></td></tr>
+<tr><td colspan=2><input type='submit' value='SIMPAN'></td></tr>
+</table>
+</form>";
+?>
